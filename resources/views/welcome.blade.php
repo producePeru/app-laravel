77544fb7f618cfcae27f1c765d665e83ba9a33ea
JL <jloo6778@gmail.com>
2023-12-13 18:28:48 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Produce PERU</title>
+        <title>Produce PERU | David</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
