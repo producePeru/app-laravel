64878f717be74fa02d23eaf59c540e5a4801793c
David <quispesistema@gmail.com>
2024-09-27 22:51:20 -0500

    planes

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
+    | Cross-Origin Resource Sharing (CORS) Configuration dr!
     |--------------------------------------------------------------------------
     |
     | Here you may configure your settings for cross-origin resource sharing
