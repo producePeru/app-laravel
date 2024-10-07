49197d35e62ffd44f753a9ac94f83a595d79ca52
David <quispesistema@gmail.com>
2024-10-07 12:45:44 -0500

    spatie

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9c491b8531eec05ba41a11d9276a5749::getLoader();
+return ComposerAutoloaderInit9f08325c360f2f9f4ccf3bcf28c5d10a::getLoader();
