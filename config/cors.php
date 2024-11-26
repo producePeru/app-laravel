4115f4241e1196c930d21a0d381a5e101914a9b2
HP PRODUCE <produce@gmail.com>
2024-11-25 19:51:12 -0500

    api

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['https://programa.soporte-pnte.com'],
+    'allowed_origins' => ['https://programa.soporte-pnte.com', 'https://encuesta.soporte-pnte.com/'],
 
     'allowed_origins_patterns' => [],
 
