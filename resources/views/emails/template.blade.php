e4d3dfecdbcd80dc0f9a66d40f4fac5b187d58ce
David Quispe <jloo6778@gmail.com>
2025-06-06 16:01:13 -0500

    app

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -5,13 +5,20 @@
     <title>Programa Nacional Tu Empresa</title>
     <style>
         .masive {
+            border: 1px solid #ffffff;
+            margin: 20px auto;
+            box-shadow: -1px 0px 10px #ccc;
             max-width: 800px;
-            padding: 10px 30px;
+            padding: 20px 40px;
             border-radius: 10px;
         }
 
         .masive br {
             display: none;
+        }
+
+        .masive h2 {
+            line-height: 1.3;
         }
     </style>
 </head>
