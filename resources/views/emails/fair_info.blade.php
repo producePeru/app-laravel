c50b10df3a0c44eb16277191cc005cac26f287c7
David Quispe <jloo6778@gmail.com>
2025-06-11 18:01:16 -0500

    sed

--- resources/views/emails/fair_info.blade.php
+++ resources/views/emails/fair_info.blade.php
@@ -7,7 +7,7 @@
                     style="background-color:#ffffff; border-radius:8px; overflow:hidden;">
                     <!-- Header -->
                     <tr>
-                        <td align="center" style="padding: 20px;">
+                        <td align="center" style="padding: 50px 20px 20px 20px;">
                             <img src="https://apituempresa.soporte-pnte.com/images/tuempresalogo.png"
                                 alt="Programa Tu Empresa" width="120" style="display:block;">
                         </td>
@@ -25,18 +25,14 @@
                     <!-- Message -->
                     <tr>
                         <td align="center"
-                            style="font-family: Arial, sans-serif; font-size: 16px; color: #666; padding: 0 40px 30px; line-height: 1.4">
-                            Hola, {{ $messageContent }}
+                            style="font-family: Arial, sans-serif; font-size: 16px; color: #666; padding: 0 40px 30px; line-height: 1.7">
+                            {{ $messageContent }}
                         </td>
                     </tr>
 
                     <tr>
                         <td align="center" style="padding: 20px;">
-                            <a href="{{ asset('storage/entradas/' . basename($qrPath)) }}"
-                                style="background-color: #e31d1a; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;"
-                                target="_blank">
-                                Descargar entrada
-                            </a>
+                           <span>Hemos adjuntado tu entrada para el evento en este correo.</span>
                         </td>
                     </tr>
 
@@ -44,7 +40,7 @@
                     <!-- Footer -->
                     <tr>
                         <td align="center"
-                            style="padding: 20px; font-family: Arial, sans-serif; font-size: 12px; color: #aaa;">
+                            style="padding: 20px 20px 50px 20px; font-family: Arial, sans-serif; font-size: 12px; color: #aaa;">
                             © Programa Nacional Tu Empresa.
                         </td>
                     </tr>
