05dc5f20b31b9124799af4687389cdacd3f2b6ed
Elizabeth Holmes <eliza@pnte.com>
2025-09-01 15:13:31 -0500

    app

--- app/Mail/CertificadoEnviadoMail.php
+++ app/Mail/CertificadoEnviadoMail.php
@@ -25,8 +25,9 @@ class CertificadoEnviadoMail extends Mailable
         $filePath = storage_path('app/public/certificados/' . $this->pdfFile);
 
         return $this->view('emails.emailCertificadoEnviado')
-                    ->subject('Constancia de participación de curso del Ministerio de la Producción')
-                    ->with(['email' => $this->email])
-                    ->attach($filePath);
+            ->subject('Constancia de participación de curso del Ministerio de la Producción')
+            ->with(['email' => $this->email])
+            ->attach($filePath)
+            ->cc('capacitaciones_tuempresa@produce.gob.pe');
     }
 }
