329bee10125e92b68a49ad82731ac142c14a34b9
David <quispesistema@gmail.com>
2026-01-07 06:39:39 -0500

    mujer produce 26

--- routes/api/mp.php
+++ routes/api/mp.php
@@ -7,49 +7,51 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(MujerProduceController::class)->group(function () {
 
-  Route::post('register-capacitador', 'registerCapacitador');
+    Route::post('register-capacitador', 'registerCapacitador');
 
-  Route::get('all-capacitadores', 'allCapacitadores');
+    Route::get('all-capacitadores', 'allCapacitadores');
 
-  Route::post('store-event', 'mpStoreEvent');
+    Route::post('store-event', 'mpStoreEvent');
 
-  Route::get('index-events', 'mpIndexEvents');
+    Route::get('index-events', 'mpIndexEvents');
 
-  Route::put('update-event/{id}', 'mpUpdateEvent');
+    Route::put('update-event/{id}', 'mpUpdateEvent');
 
-  Route::get('attendance-event/{slug}', 'mpAttendance');
+    Route::get('attendance-event/{slug}', 'mpAttendance');
 
-  Route::post('create-question-diagnostic', 'createQuestionDiagnostic');
+    Route::post('create-question-diagnostic', 'createQuestionDiagnostic');
 
-  Route::get('get-question-diagnostic', 'getQuestionDiagnostic');
+    Route::get('get-question-diagnostic', 'getQuestionDiagnostic');
 
-  Route::put('update-question-diagnostic/{id}', 'updateQuestionDiagnostic');
+    Route::put('update-question-diagnostic/{id}', 'updateQuestionDiagnostic');
 
-  Route::put('update-status/{id}', 'updateStatus');
+    Route::put('update-status/{id}', 'updateStatus');
 
-  Route::get('participant-diagnostic', 'mpIndexParticipantDiagnostic');
+    Route::get('participant-diagnostic', 'mpIndexParticipantDiagnostic');
+
+    Route::put('update-order', 'updateOrder');
 });
 
 
 Route::controller(FormularioPublicoController::class)->group(function () {
 
-  Route::post('ruc-number/{ruc}', 'checkRucNumber');
+    Route::post('ruc-number/{ruc}', 'checkRucNumber');
 
-  Route::post('dni-number/{dni}', 'checkDniNumber');
+    Route::post('dni-number/{dni}', 'checkDniNumber');
 
-  Route::post('register-participant', 'registerParticipant');
+    Route::post('register-participant', 'registerParticipant');
 
-  Route::post('register-attendance', 'registerAttendance');
+    Route::post('register-attendance', 'registerAttendance');
 
-  Route::get('info-event-public/{slug}', 'infoEventPublic');
+    Route::get('info-event-public/{slug}', 'infoEventPublic');
 
-  Route::get('diagnostic-questions', 'diagnosticQuestions');
+    Route::get('diagnostic-questions', 'diagnosticQuestions');
 
-  Route::get('question-diagnostic', 'getQuestionDiagnostic');
+    Route::get('question-diagnostic', 'getQuestionDiagnostic');
 
-  Route::post('register-consulting', 'registerConsulting');     // inicia diagnostico 
+    Route::post('register-consulting', 'registerConsulting');     // inicia diagnostico
 
-  Route::post('register-diagnostic-response', 'registerDiagnosticResponse');
+    Route::post('register-diagnostic-response', 'registerDiagnosticResponse');
 });
 
 // mp
