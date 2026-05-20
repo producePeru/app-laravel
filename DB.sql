d0ed0e5ceb7c916c1cd2a46bd713411303cc0cb9
Dios es amor <riosdeaguaviva@correo.com>
2026-05-19 21:55:24 -0500

    db

--- DB.sql
+++ DB.sql
@@ -212,14 +212,14 @@ CREATE TABLE preguntas(
 
 
 
-ALTER TABLE actividades_pnte
-ADD COLUMN resultados TEXT
-AFTER reprogramado_por_id;
+-- ALTER TABLE actividades_pnte
+-- ADD COLUMN resultados TEXT
+-- AFTER reprogramado_por_id;
 
 
-ALTER TABLE actividades_pnte
-ADD COLUMN activo TINYINT(1) NOT NULL DEFAULT 0
-AFTER resultados;
+-- ALTER TABLE actividades_pnte
+-- ADD COLUMN activo TINYINT(1) NOT NULL DEFAULT 0
+-- AFTER resultados;
 
 
 
