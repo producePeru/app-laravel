6dfc622711bdc78e8ef8ed21b2bf574262d9c816
Elizabeth Holmes <eliza@pnte.com>
2026-06-10 17:56:10 -0500

    eventos pp

--- routes/api/pp093.php
+++ routes/api/pp093.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Pnte\PpCapacitadorController;
+use App\Http\Controllers\Pnte\ActividadPublicPnteController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PpCapacitadorController::class)->group(function () {
@@ -24,6 +25,9 @@ Route::controller(PpCapacitadorController::class)->group(function () {
 Route::controller(ActividadPublicPnteController::class)->group(function () {
     
     Route::POST('register-mype', 'registerBusinessManPP093');
+
+    Route::POST('check-course', 'checkToCoursePp093');
+
 });
 
 // pp093-public
